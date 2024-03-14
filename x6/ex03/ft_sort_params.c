0c9afe4f21f888bcb1ffbc92c568ef74090895f6
AHMET ASAAD HAMMOUD <88238215+ahmed741321@users.noreply.github.com>
2024-03-14 07:16:41 +0100

    Add files via upload

---
+++ x6/ex03/ft_sort_params.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aasaad-h <aasaad-h@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/10 10:38:56 by aasaad-h          #+#    #+#             */
+/*   Updated: 2024/03/10 10:39:50 by aasaad-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+	k = 1;
+	while (k < argc)
+	{
+		ft_putstr(argv[k++]);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
